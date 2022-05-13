59d269a2cc04b98b2b685a1f3d2ca823c822c483
Antonio <antonio18casta@aol.com>
2022-05-13 18:02:06 -0400

    commit tree cebe42bc462f1dfc8487ee24dfb0756c73e4ea0f

---
+++ index.js
@@ -0,0 +1,40 @@
+var a, b
+//1
+function add(a,b) {
+    return a + b;
+}
+
+//2
+function subtract(a,b) {
+    return a - b;
+}
+
+//3
+function multiply(a,b) {
+    return a * b;
+}
+
+//4:8
+function divide(a,b) {
+    return a / b;
+}
+
+//9
+function increment(n) {
+    return n + 1;
+}
+
+//10
+function decrement(n) {
+    return n - 1;
+}
+
+//11:12
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+//13
+function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
